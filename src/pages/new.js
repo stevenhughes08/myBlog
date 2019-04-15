bb3576ef041a0e96676f23dec123476f4f5a53fc
stevenhughes08 <stevenhughes08@gmail.com>
2019-04-15 14:18:56 -0400

    added new.js

---
+++ src/pages/new.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const NewPage = () => (
+    <Layout>
+        <h1>I am a new NewPage!</h1>
+    </Layout>
+)
+
+export default NewPage
