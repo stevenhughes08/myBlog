e54ca63703c7a93c1d7df95fad9656271ec38b11
stevenhughes08 <stevenhughes08@gmail.com>
2019-05-05 14:44:40 -0400

    Added search to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Search from "../components/SearchContainer"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="CodeTape" keywords={[`gatsby`, `application`, `react`]} />
+    
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div>
+    <h1 style={{ marginTop: `3em`, textAlign: `center` }}>
+      Search this Blog
+    </h1>
+    <div>
+      <Search />
+    </div>
+  </div>
+    
   </Layout>
 )
 
